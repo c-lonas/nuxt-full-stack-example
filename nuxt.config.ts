db511da814ab674e03ce2d307fa9d63e041b0169
Chase Lonas <chaselonas@gmail.com>
2024-03-22 18:44:06 -0400

    placeholder supabase config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: ['@nuxt/ui-pro'],
-  modules: ['@nuxt/ui'],
+  modules: [
+    '@nuxt/ui',
+    '@nuxtjs/supabase'
+  ],
+  supabase: {
+    url: 'YOUR_SUPABASE_URL', // Replace with your Supabase Project URL
+    key: 'YOUR_SUPABASE_ANON_KEY' // Replace with your Supabase anon key
+  },
 })
